1cf2f40f12e350d52d8db73b77849a5661f28d15
Drake <dduerdo1@depaul.edu>
2022-04-13 21:24:49 -0500

    Catalog.java

--- CSC553-lab1/src/java/simpledb/Catalog.java
+++ CSC553-lab1/src/java/simpledb/Catalog.java
@@ -6,35 +6,39 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-/**
- * The Catalog keeps track of all available tables in the database and their
- * associated schemas.
- * For now, this is a stub catalog that must be populated with tables by a
- * user program before it can be used -- eventually, this should be converted
- * to a catalog that reads a catalog table from disk.
- */
 
 public class Catalog {
+    private class Table {
+        public Table(String name, DbFile dbfile, String pkeyField) {
+            this.Name = name;
+            this.File = dbfile;
+            this.PkeyField = pkeyField;
+        }
+        public DbFile getDbFile() {
+            return this.File;
+        }
+        public String getPkeyField() {
+            return this.PkeyField;
+        }
+        public String getName() {
+            return this.Name;
+        }
+        private String Name;
+        private DbFile File;        
+        private String PkeyField;
+    }
+    private HashMap<String,Table> NameHash;
+    private HashMap<Integer,Table> IdHash;
 
-    /**
-     * Constructor.
-     * Creates a new, empty catalog.
-     */
     public Catalog() {
-        // some code goes here
+        this.NameHash = new HashMap<String,Table>();
+        this.IdHash = new HashMap<Integer,Table>();
     }
 
-    /**
-     * Add a new table to the catalog.
-     * This table's contents are stored in the specified DbFile.
-     * @param file the contents of the table to add;  file.getId() is the identfier of
-     *    this file/tupledesc param for the calls getTupleDesc and getFile
-     * @param name the name of the table -- may be an empty string.  May not be null.  If a name
-     * @param pkeyField the name of the primary key field
-     * conflict exists, use the last table to be added as the table for a given name.
-     */
     public void addTable(DbFile file, String name, String pkeyField) {
-        // some code goes here
+        Table data = new Table(name,file,pkeyField);
+        this.NameHash.put(name,data);
+        this.IdHash.put(file.getId(),data);
     }
 
     public void addTable(DbFile file, String name) {
@@ -57,8 +61,12 @@ public class Catalog {
      * @throws NoSuchElementException if the table doesn't exist
      */
     public int getTableId(String name) throws NoSuchElementException {
-        // some code goes here
-        return 0;
+        Table match = this.NameHash.get(name);
+        if(match == null) throw new NoSuchElementException();
+        else {
+        	DbFile file = match.getDbFile();
+        	return file.getId();
+        }
     }
 
     /**
@@ -68,8 +76,12 @@ public class Catalog {
      * @throws NoSuchElementException if the table doesn't exist
      */
     public TupleDesc getTupleDesc(int tableid) throws NoSuchElementException {
-        // some code goes here
-        return null;
+    	Table match = this.IdHash.get(tableid);
+    	if(match == null) throw new NoSuchElementException();
+    	else {
+    		DbFile file = match.getDbFile();
+    		return file.getTupleDesc();
+    	}
     }
 
     /**
@@ -79,28 +91,32 @@ public class Catalog {
      *     function passed to addTable
      */
     public DbFile getDbFile(int tableid) throws NoSuchElementException {
-        // some code goes here
-        return null;
+    	Table match = this.IdHash.get(tableid);
+    	if(match == null) throw new NoSuchElementException();
+    	else return match.getDbFile();
     }
 
     public String getPrimaryKey(int tableid) {
-        // some code goes here
-        return null;
+    	Table match = this.IdHash.get(tableid);
+    	if (match == null) throw new NoSuchElementException();
+    	else return match.getPkeyField();
     }
 
     public Iterator<Integer> tableIdIterator() {
-        // some code goes here
-        return null;
+        Set<Integer> keys = this.IdHash.keySet();
+        return keys.iterator();
     }
 
     public String getTableName(int id) {
-        // some code goes here
-        return null;
+    	Table match = this.IdHash.get(id);
+    	if(match == null) throw new NoSuchElementException();
+    	else return match.getName();
     }
     
     /** Delete all tables from the catalog */
     public void clear() {
-        // some code goes here
+    	this.IdHash.clear();
+    	this.NameHash.clear();
     }
     
     /**
